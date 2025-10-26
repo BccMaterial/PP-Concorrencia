cfe819e17e4e190e41e196b1ce62d2e4f79b0a2b
viniciusegydio <egydiovinicius022@gmail.com>
2025-10-26 18:37:20 -0300

    fixes comunicacao_threads.nim

--- src/comunicacao_threads.nim
+++ src/comunicacao_threads.nim
@@ -1,18 +1,11 @@
-import std/threads, std/os
+import std/threadpool
 
-#Diferente de C ou C#, cada thread em Nim tem seu próprio heap que é gerenciado por um garbage colector independente
-#Objetos alocados como seq ou string pertencem apenas a thread responsável por sua criação
+#Função executada na thread
+proc worker(msg: string): string {.thread.} =
+  return "Worker recebeu: " & msg
 
-#Comunicação por threads:
-var canal: Channel[string] #Channel[T] é uma fila que permite a comuniacação entre threads de maneira segura
-canal.open() #Faz a inicialização do canal para permitir o uso de send ou recv
- 
-proc worker() {.thread.} =
-  let msg = canal.recv() #Faz o bloqueamento da thread até que ocorra o envio de uma mensagem para o canal com canal.send
-  echo "Recebeu: ", msg #OBS: caso a mensagem não chegue, a thread é bloqueada indefinidamente
+#Cria uma thread e retorna um FlowVar
+let future = spawn worker("Olá mundo!")
 
-var t: Thread[void] #Armazena o identificador da thread, onde a mesma não retorna nada. Serve apenas para fazer jointhread
-createThread(t, worker) #Cria uma nova thread que entra em worker() e fica bloqueada em recv() enquanto espera a mensagem
-
-ch.send("Olá mundo!") #Cópia segura da string
-joinThread(t) 
+#Bloqueia a thread principal até que o resultado esteja pronto
+echo ^future #"^" espera que a FlowVar esteja pronta para retornar o valor ou passar uma excessão, caso a task falhe.
